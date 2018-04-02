db5b30675c7193b3af7781d2ff14d600179fe08a
ChristopherALee <christopheradamlee@gmail.com>
2018-04-02 10:08:13 -0400

    Completed 'Learn Express Routes: Adding animals Routes'

--- learn-express-notes/1_express_routes/app.js
+++ learn-express-notes/1_express_routes/app.js
@@ -69,7 +69,57 @@ app.delete("/expressions/:id", (req, res, next) => {
   }
 });
 
-// 'PORT' is where the server is listening from and the callback in the 2nd argument is invoked when the server is running and ready to receive responses.
-app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
+// Animal routes
+const animals = [];
+
+app.get("/animals", (req, res, next) => {
+  res.send(animals);
 });
+
+app.get("/animals/:id", (req, res, next) => {
+  const foundAnimal = getElementById(req.params.id, animals);
+
+  if (foundAnimal !== -1) {
+    res.send(foundAnimal);
+  } else {
+    res.status(404).send("Animal not found");
+  }
+});
+
+app.put("/animals/:id", (req, res, next) => {
+  const animalIndex = getIndexById(req.params.id, animals);
+
+  if (animalIndex !== -1) {
+    updateElement(req.params.id, req.query, animals);
+    res.send(animals[animalIndex]);
+  } else {
+    res.status(404).send();
+  }
+});
+
+app.post("/animals", (req, res, next) => {
+  const receivedAnimal = createElement("animals", req.query);
+
+  if (receivedAnimal) {
+    animals.push(receivedAnimal);
+    res.status(201).send(receivedAnimal);
+  } else {
+    res.status(400).send();
+  }
+});
+
+app.delete("/animals/:id", (req, res, next) => {
+  const foundAnimal = getIndexById(req.params.id, animals);
+
+  if (foundAnimal !== -1) {
+    animals.splice(foundAnimal, 1);
+    res.status(204).send();
+  } else {
+    res.status(404).send();
+  }
+});
+
+app.app // 'PORT' is where the server is listening from and the callback in the 2nd argument is invoked when the server is running and ready to receive responses.
+  .listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+  });
