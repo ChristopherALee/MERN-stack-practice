299efc542cbaeaea5017e85cd7b595c0ac420643
ChristopherALee <christopheradamlee@gmail.com>
2018-04-12 12:25:27 -0400

    Completed 'Learn Express: Middleware - Code Challenge 1'

---
+++ learn-express-notes/2_middleware/3_coding_challenges/app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+
+// Finish this function and use it in the routes below
+
+const sendFruitResponse = (req, res, next) => {};
+
+app.get("/whatis/apple", (req, res, next) => {
+  res.send("fruit");
+});
+
+app.get("/whatis/orange", (req, res, next) => {
+  res.send("fruit");
+});
+
+app.get("/whatis/banana", (req, res, next) => {
+  res.send("fruit");
+});
+
+// Answer
+const sendFruitResponse = (req, res, next) => {
+  res.send("fruit");
+};
+
+app.get("/whatis/apple", sendFruitResponse);
+
+app.get("/whatis/orange", sendFruitResponse);
+
+app.get("/whatis/banana", sendFruitResponse);
