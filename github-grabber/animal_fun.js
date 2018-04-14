e1f5a7d769fdc2733976385d2e3e12498893c742
ChristopherALee <christopheradamlee@gmail.com>
2018-04-14 15:32:20 -0400

    Created a filesystem (fs) writefile method to create example txt and contents within it

--- github-grabber/animal_fun.js
+++ github-grabber/animal_fun.js
@@ -9,3 +9,13 @@ fs.readFile("./animals.txt", "utf-8", (err, data) => {
 
   console.log(data);
 });
+
+// Run this code and you'll see the new file example.txt written in the same directory as our animal_fun.js file. If a file already exists as the first argument, it will be overwritten so be careful.
+fs.writeFile("./example.txt", "I will be written to example.txt", err => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  console.log("file successfully written");
+});
