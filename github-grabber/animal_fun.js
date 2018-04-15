7757cdcb8b3fa96fbfe99266facc6a7eed7383c6
ChristopherALee <christopheradamlee@gmail.com>
2018-04-15 12:17:59 -0400

    Practiced access to node global variable 'process'

--- github-grabber/animal_fun.js
+++ github-grabber/animal_fun.js
@@ -19,3 +19,11 @@ fs.writeFile("./example.txt", "I will be written to example.txt", err => {
 
   console.log("file successfully written");
 });
+
+// Eventually we'll be parsing input from HTTP requests, but to start let's explore how to pass information into our script from the command line. Node provides a global process object which will allow us to pass arbitrary arguments from our terminal.
+// console.log(process);
+
+// Wow, that's a lot of stuff! process contains loads of information, so we'll narrow it down by accessing the argv key. console.log(process.argv). We should see an array with two arguments: the absolute paths of the Node executable and the file.
+console.log(process.argv);
+
+// Try adding some additional words after node animal_fun.js and see how it comes through. For example node animal_fun.js argv_index_2 argv_index_3 potato. We'll have access to those additional arguments in our script by bracketing into the process.argv array starting at process.argv[2].
