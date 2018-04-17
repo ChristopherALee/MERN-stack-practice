832eea20047ca6904b8d445260af5337f407a90b
ChristopherALee <christopheradamlee@gmail.com>
2018-04-17 09:46:51 -0400

    Handles query searches via server to read animals file

--- github-grabber/animal_fun.js
+++ github-grabber/animal_fun.js
@@ -1,4 +1,7 @@
 const fs = require("fs");
+const http = require("http");
+const qs = require("querystring");
+const cache = {};
 
 // async function
 fs.readFile("./animals.txt", "utf-8", (err, data) => {
@@ -57,3 +60,49 @@ fs.readFile("./animals.txt", "utf-8", (err, data) => {
     console.log(`Successfully created ${animalLetter}_animals.txt`);
   });
 });
+
+function selectAnimals(animalString, animalLetter) {
+  return animalString
+    .split("\n")
+    .filter(animal => animal.startsWith(animalLetter))
+    .join("\n");
+}
+
+const animalServer = http.createServer((req, res) => {
+  const query = req.url.split("?")[1];
+  if (query !== undefined) {
+    const animalLetter = qs.parse(query).letter.toUpperCase();
+
+    if (cache[animalLetter] !== undefined) {
+      res.end(cache[animalLetter]);
+    }
+
+    if (animalLetter !== undefined) {
+      fs.readFile("./animals.txt", "utf-8", (err, data) => {
+        if (err) {
+          console.log(err);
+          res.end("IT WENT POORLY");
+          return;
+        }
+        const animals = selectAnimals(data, animalLetter);
+        cache[animalLetter] = animals;
+        res.end(animals);
+      });
+    }
+  } else {
+    if (cache["animals"] !== undefined) {
+      res.end(cache["animals"]);
+    }
+    fs.readFile("./animals.txt", "utf-8", (err, data) => {
+      if (err) {
+        console.log(err);
+        res.end("IT WENT POORLY");
+        return;
+      }
+      cache["animals"] = data;
+      res.end(data);
+    });
+  }
+});
+
+animalServer.listen(8000, () => console.log("I'm listening on port 8000"));
