fd94b250f36c606b004396c284997de1b6b39ae0
ChristopherALee <christopheradamlee@gmail.com>
2018-08-11 18:18:36 -0400

    writable streams

--- net-ninja-nodejs/stream.js
+++ net-ninja-nodejs/stream.js
@@ -3,11 +3,13 @@ const fs = require("fs");
 
 // will send chunk package numbers
 const myReadStream = fs.createReadStream(`${__dirname}/readMe.txt`);
+const myWriteStream = fs.createWriteStream(`${__dirname}/writeMe.txt`);
 
 // will send chunks with actual file contents
 // const myReadStream = fs.createReadStream(`${__dirname}/readMe.txt`, "utf8");
 
 myReadStream.on("data", chunk => {
   console.log(`new chunk received:`);
-  console.log(chunk);
+
+  myWriteStream.write(chunk);
 });
