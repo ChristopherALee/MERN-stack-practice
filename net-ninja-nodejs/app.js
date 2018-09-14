3b8c00145fbadd8c5c1b367ae7fe223de4652eac
ChristopherALee <christopheradamlee@gmail.com>
2018-09-14 14:06:31 -0400

    section of code to serve JSON data via res.writeHead

--- net-ninja-nodejs/app.js
+++ net-ninja-nodejs/app.js
@@ -3,11 +3,24 @@ const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   console.log("request was made: " + req.url);
-  res.writeHead(200, { "Content-Type": "text/html" });
 
-  const myReadStream = fs.createReadStream(`${__dirname}/index.html`, "utf8");
+  // serving html
+  // res.writeHead(200, { "Content-Type": "text/html" });
 
-  myReadStream.pipe(res);
+  // const myReadStream = fs.createReadStream(`${__dirname}/index.html`, "utf8");
+
+  // myReadStream.pipe(res);
+
+  // serving JSON data
+  res.writeHead(200, { "Content-Type": "application/json" });
+
+  const myObj = {
+    name: "Ryu",
+    job: "Ninja",
+    age: 29
+  };
+
+  res.end(JSON.stringify(myObj));
 });
 
 server.listen(3000, "127.0.0.1");
