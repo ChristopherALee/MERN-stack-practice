1d79a5c2a15aa392547ec3aaa9708e889dba7201
ChristopherALee <christopheradamlee@gmail.com>
2018-09-14 14:03:41 -0400

    altered server setup to write to the head and serve html as opposed to plain text

--- net-ninja-nodejs/app.js
+++ net-ninja-nodejs/app.js
@@ -3,9 +3,9 @@ const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   console.log("request was made: " + req.url);
-  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.writeHead(200, { "Content-Type": "text/html" });
 
-  const myReadStream = fs.createReadStream(`${__dirname}/readMe.txt`, "utf8");
+  const myReadStream = fs.createReadStream(`${__dirname}/index.html`, "utf8");
 
   myReadStream.pipe(res);
 });
