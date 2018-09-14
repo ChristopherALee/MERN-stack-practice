1961d44abf2cb99ee239ab8ca8ff4251c9878daa
ChristopherALee <christopheradamlee@gmail.com>
2018-09-14 14:18:40 -0400

    started basic routes

--- net-ninja-nodejs/app.js
+++ net-ninja-nodejs/app.js
@@ -4,6 +4,10 @@ const fs = require("fs");
 const server = http.createServer((req, res) => {
   console.log("request was made: " + req.url);
 
+  // serving plain-text
+  // res.writeHead(200, { "Content-Type": "plain" });
+  // res.end("feed me popcorn");
+
   // serving html
   // res.writeHead(200, { "Content-Type": "text/html" });
 
@@ -12,15 +16,43 @@ const server = http.createServer((req, res) => {
   // myReadStream.pipe(res);
 
   // serving JSON data
-  res.writeHead(200, { "Content-Type": "application/json" });
+  // res.writeHead(200, { "Content-Type": "application/json" });
 
-  const myObj = {
-    name: "Ryu",
-    job: "Ninja",
-    age: 29
-  };
+  // const myObj = {
+  //   name: "Ryu",
+  //   job: "Ninja",
+  //   age: 29
+  // };
 
-  res.end(JSON.stringify(myObj));
+  // res.end(JSON.stringify(myObj));
+
+  if (req.url === "/home" || req.url === "/") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+
+    fs.createReadStream(`${__dirname}/index.html`).pipe(res);
+  } else if (req.url === "/contact") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+
+    fs.createReadStream(`${__dirname}/contact.html`).pipe(res);
+  } else if (req.url === "/api/ninjas") {
+    const ninjas = [
+      {
+        name: "Ryu",
+        age: 29
+      },
+      {
+        name: "Yoshi",
+        age: 32
+      }
+    ];
+
+    res.writeHead(200, { "Content-Type": "application/json" });
+
+    res.end(JSON.stringify(ninjas));
+  } else {
+    res.writeHead(404, { "Content-Type": "text/html" });
+    fs.createReadStream(`${__dirname}/404.html`).pipe(res);
+  }
 });
 
 server.listen(3000, "127.0.0.1");
