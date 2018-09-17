282b388c323bbf7d44bab354d1db157d3706aa86
ChristopherALee <christopheradamlee@gmail.com>
2018-09-17 18:48:13 -0400

    created partial template

--- net-ninja-nodejs/app2.js
+++ net-ninja-nodejs/app2.js
@@ -7,19 +7,27 @@ app.set("view engine", "ejs");
 app.get("/", (req, res) => {
   // res.send("this is the homepage");
 
-  res.sendFile(`${__dirname}/index.html`);
+  // res.sendFile(`${__dirname}/index.html`);
+
+  res.render("index");
 });
 
 app.get("/contact", (req, res) => {
   // res.send("this is the contact page");
 
-  res.sendFile(`${__dirname}/contact.html`);
+  // res.sendFile(`${__dirname}/contact.html`);
+
+  res.render("contact");
 });
 
 app.get("/profile/:name", (req, res) => {
   // res.send(`You requested to see a profile with the id of ${req.params.id}`);
 
-  res.render("profile", { person: req.params.name });
+  const data = {
+    age: 29,
+    job: "ninja"
+  };
+  res.render("profile", { person: req.params.name, data });
 });
 
 app.listen(3000);
