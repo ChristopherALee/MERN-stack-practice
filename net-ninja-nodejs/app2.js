75e3e441a9c834ebb48253002fb73a44b11f1618
ChristopherALee <christopheradamlee@gmail.com>
2018-09-17 18:28:50 -0400

    installed ejs and using it to send data towards ejs files from the server requests

--- net-ninja-nodejs/app2.js
+++ net-ninja-nodejs/app2.js
@@ -2,16 +2,24 @@ const express = require("express");
 
 const app = express();
 
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-  res.send("this is the homepage");
+  // res.send("this is the homepage");
+
+  res.sendFile(`${__dirname}/index.html`);
 });
 
 app.get("/contact", (req, res) => {
-  res.send("this is the contact page");
+  // res.send("this is the contact page");
+
+  res.sendFile(`${__dirname}/contact.html`);
 });
 
-app.get("/profile/:id", (req, res) => {
-  res.send(`You requested to see a profile with the id of ${req.params.id}`);
+app.get("/profile/:name", (req, res) => {
+  // res.send(`You requested to see a profile with the id of ${req.params.id}`);
+
+  res.render("profile", { person: req.params.name });
 });
 
 app.listen(3000);
